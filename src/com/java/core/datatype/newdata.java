39b60a0d947ac87a71e0210b630bab6732c35525
PRASHANT <PRASHANT@192.168.1.97>
2026-04-18 16:56:19 +0530

    All datatype commit

---
+++ src/com/java/core/datatype/newdata.java
@@ -0,0 +1,25 @@
+package com.java.core.datatype;
+
+public class newdata {
+	public static void main(String[] args) {
+		
+		int ww = 12;
+		double vv = 123;
+		short age = 19;
+		long phonenumber = 12344321;
+		byte kv = 122;
+		float p = 23.55f;
+		char alpha = 'a';
+		boolean red = true;
+		
+		System.out.println(ww);
+		System.out.println(vv);
+		System.out.println(age);
+		System.out.println(phonenumber);
+		System.out.println(kv);
+		System.out.println(p);
+		System.out.println(alpha);
+		System.out.println(red);
+	}
+
+}
