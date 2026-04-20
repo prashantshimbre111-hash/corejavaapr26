3d48bb12964bc54cfc8f46a7098cdd47fa03ea97
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-20 14:12:19 +0530

    arithematic operators commit implemented

---
+++ src/com/java/core/datatype/arithematic.java
@@ -0,0 +1,21 @@
+package com.java.core.datatype;
+
+public class arithematic {
+	public static void main(String[] args) {
+		
+		int a = 12;
+		int b = 54;
+		int c = 65;
+		int d = 21;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a+b+c+c);
+		System.out.println(a*c*d);
+		System.out.println((a+d) + (c+b));
+		System.out.println((a*b) - (c*d));
+	}
+
+}
