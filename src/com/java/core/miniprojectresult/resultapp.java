eae50b49f5124b9a3677f9b77dcfc5091d82eeba
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-20 14:29:19 +0530

    result app commit implemented

--- src/com/java/core/miniprojectresult/resultapp.java
+++ src/com/java/core/miniprojectresult/resultapp.java
@@ -7,22 +7,26 @@ public class resultapp {
 		rohitmarksheet r = new rohitmarksheet();
 		int rohitresult = (r.rohitmark*100)/r.totalsubmark;
 		
-		System.out.println("rohit percentage : " + rohitresult);
+		System.out.println("rohit result in percentage  : " + rohitresult);
 		
 		prashantmarksheet p = new prashantmarksheet();
 		
 		int prashantresult = (p.totalmarks*100)/p.totalsubmarks;
-		System.out.println("prashant percentage : " + prashantresult);
+		System.out.println("prashant result in percentage  : " + prashantresult);
 		
 		rohanmarksheet rr = new rohanmarksheet();
 		
 		int rohanresult = (rr.totalmarks*100)/rr.totalsubmarks;
-		System.out.println("rohan percentage : "  + rohanresult);
+		System.out.println("rohan result in percentage  : "  + rohanresult);
 		
 		romamarksheet rrr = new romamarksheet();
 		
 		int romaresult = (rrr.totalmarks*100)/rrr.totalsubmarks;
-		System.out.println("roma percentage : " + romaresult);
+		System.out.println("roma result in percentage  : " + romaresult);
+		
+		shivay s = new shivay();
+		int result = (s.totalmarks*100)/s.totalsubtotal;
+		System.out.println("shivay result in percentage : " + result);
 		
 	}
 }
---
+++ src/com/java/core/miniprojectresult/shivay.java
@@ -0,0 +1,22 @@
+package com.java.core.miniprojectresult;
+
+public class shivay {
+		
+		int mar = 87;
+		int hin = 89;
+		int eng = 86;
+		int geo = 41;
+		int his = 39;
+		int totalmarks = mar + hin + eng + geo + his;
+		
+		
+		int martotal = 100;
+		int hintotal = 100;
+		int engtotal = 100;
+		int geototal = 50;
+		int histotal = 50;
+		int totalsubtotal = martotal + hintotal + engtotal + geototal + histotal;
+		
+	}
+
+
