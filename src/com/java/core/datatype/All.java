87393f77ebff3cca148d52f911612b007f8ee45d
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-21 11:24:08 +0530

    primitive datatype commit implemented

---
+++ src/com/java/core/datatype/All.java
@@ -0,0 +1,27 @@
+package com.java.core.datatype;
+
+public class All {
+	
+	public static void main(String[] args) {
+		
+		int a = 12;
+		double b = 432.75;
+		short age = 42;
+		long contact = 232334455;
+		byte kb = 1;
+		float p = 54.65f;
+		char A = 'a';
+		boolean data = true;
+		
+		System.out.println("this is int : " + a);
+		System.out.println("this is double : " + b);
+		System.out.println("this is short : " + age);
+		System.out.println("this is long : " + contact);
+		System.out.println("thiss is byte : " + kb);
+		System.out.println("this is float : " + p);
+		System.out.println("this is char : " + A);
+		System.out.println("this is boolean : " + data);
+		
+	}
+
+}
