e905eea621304759a8439fde5a5b32996555faa5
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-27 18:55:59 +0530

    Implement ForLoop

---
+++ src/com/java/core/Loops/ForLoop.java
@@ -0,0 +1,24 @@
+package com.java.core.Loops;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//innitialization
+		//condition
+		//increament/decreament
+		
+		System.out.println("start");
+		
+		for ( int i = 0 ; i < 5 ; i++ ) {
+			
+			System.out.println("Hii" + i);
+		}
+		for(int i = 5 ; i > 0 ; i--){
+			System.out.println(i);
+		}
+
+	}
+
+}
