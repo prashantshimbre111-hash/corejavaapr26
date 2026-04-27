bab6c4ed3cb790e41722d7d40219da931d378e78
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-27 15:58:18 +0530

    implement result application

---
+++ src/com/java/core/entity/student.java
@@ -0,0 +1,12 @@
+package com.java.core.entity;
+
+public class student {
+	
+	public int id;
+	public String fname;
+	public String lname;
+	public int english;
+	public int math;
+	public int science;
+
+}
