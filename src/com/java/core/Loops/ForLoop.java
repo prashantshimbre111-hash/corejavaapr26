62f07b831524c6835260bdf4a68db5757058e02e
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-29 14:31:14 +0530

    we find even number from 1 to 30 with for loop

--- src/com/java/core/Loops/ForLoop.java
+++ src/com/java/core/Loops/ForLoop.java
@@ -15,7 +15,7 @@ public class ForLoop {
 			
 			System.out.println("Hii" + i);
 		}
-		for(int i = 5 ; i > 0 ; i--){
+		for(int i = 5 ; i >0 ; i--){
 			System.out.println(i);
 		}
 
---
+++ src/com/java/core/whileLoop/Example.java
@@ -0,0 +1,27 @@
+package com.java.core.whileLoop;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Q. fine even number from 1 to 30.
+
+		/*
+		 * we need to find numbers from 1 to 30. we need to find even number.
+		 * 
+		 */
+
+		// using for loop;
+
+		for (int i = 1; i <= 30; i ++) {
+
+			if (i % 2 == 0) {
+
+				System.out.println(i);
+			}
+
+		}
+	}
+
+}
