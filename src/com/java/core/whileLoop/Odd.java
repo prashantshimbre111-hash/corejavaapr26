04aa28d527e32922ea4fbff5580143bbcaf67652
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-29 14:46:27 +0530

    We find odd number with while loop.

--- src/com/java/core/whileLoop/Odd.java
+++ src/com/java/core/whileLoop/Odd.java
@@ -11,8 +11,22 @@ public class Odd {
 		//we have to find odd number from 1 to 30  with for loop.
 		
 		for(int i = 1 ; i<=30 ; i++) {
-			if(i%2==0) {System.out.println(i);}
+			if(i%2==1) {System.out.println(i);}
+		}
+		
+		System.out.println("****************************************************************************************");
+		
+		//we have to find odd number with while loop.
+		
+		int i = 1;
+		while(i<=30) {
+			if(i%2==1) {
+				System.out.println(i);
+			}
+			
+			i++;
 		}
 	}
+	
 
 }
