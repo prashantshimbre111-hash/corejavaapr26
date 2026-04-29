e567fd3a939aa3bec4c4c36c41ccc4530dad8933
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-29 16:03:27 +0530

    while loop with true implemented.

---
+++ src/com/java/core/whileLoop/whiletrue.java
@@ -0,0 +1,23 @@
+package com.java.core.whileLoop;
+
+import java.util.Scanner;
+
+public class whiletrue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+		
+		System.out.println("please enter value here : ");
+		int n = sc.nextInt();
+		
+		System.out.println(n);
+		
+		}
+
+	}
+
+}
