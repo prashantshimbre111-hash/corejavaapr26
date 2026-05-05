1236d4087af02a079ffcfca704a93034475acdbb
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-05 15:17:26 +0530

    parameterized constructor implemented.

---
+++ src/com/java/core/main/costructor/Student.java
@@ -0,0 +1,20 @@
+package com.java.core.main.costructor;
+
+public class Student {
+	
+	public String name;
+	public int age;
+	
+	public Student(String name , int age) {
+		
+		this. name = name;
+		this.age = age;
+	
+	}
+	public void printname() {
+		
+		System.out.println(name);
+		System.out.println(age);
+	}
+
+}
---
+++ src/com/java/core/main/costructor/Test.java
@@ -0,0 +1,11 @@
+package com.java.core.main.costructor;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		Student s = new Student("prashant" , 12);
+			s.printname();
+		
+	}
+
+}
