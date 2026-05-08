cd3ace9a5a40ca5a60d2f644d45079afa3df0803
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 12:18:27 +0530

    Wrapper class implemented.

---
+++ src/String/Wrapperclass.java
@@ -0,0 +1,19 @@
+package String;
+
+public class Wrapperclass {
+	
+	public static void main(String[]args) {
+		
+//		datatype
+		int a = 12;
+		
+//		object
+		Integer b = 12;
+		
+//		object
+		Integer c = new Integer(12);
+		System.out.println(c);
+		
+	}
+
+}
