8b4c30482ace9e17e05f54b648adb9f4a1ed921f
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 13:07:20 +0530

    .

--- src/com/java/core/Strings/StringTest.java
+++ src/com/java/core/Strings/StringTest.java
@@ -2,12 +2,37 @@ package com.java.core.Strings;
 
 public class StringTest {
 	
-	public static void main(String[]args) {
-		
+public static void main(String[] args) {
+	
+	
 		String day = "tuesday";
 		
+//		measure length
 		int length = day.length();
 		System.out.println(length);
+		
+//		get charecter using index
+		char c = day.charAt(2);
+		System.out.println(c);
+		
+//		converts uppercase to lowercase
+		String d = day.toLowerCase();
+		System.out.println(d);
+		
+//		converts lowercase to uppercase
+		String e = day.toUpperCase();
+		System.out.println(e);
+		
+//		check word is exist or not
+		boolean flag = day.contains("t");
+		System.out.println(flag);
+		
+//		concat two world or thingd
+		
+		String con = day.concat("abcd");
+		System.out.println(con);
+		
+		
 	}
 
 }
