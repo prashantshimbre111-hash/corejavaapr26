e0165b8bd81879f02d7eb1fc58331fdf933a1186
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-08 14:46:20 +0530

    .

--- src/Stringforloop/TestOne.java
+++ src/Stringforloop/TestOne.java
@@ -15,20 +15,34 @@ public class TestOne {
 //	d
 //	a
 //	y
-		
+
 		System.out.println(day);
 		System.out.println("we printed wednesday in for loop");
-		
-		for(int i = 0 ; i<=day.length()-1 ; i++) {
-			
+
+		for (int i = 0; i <= day.length() - 1; i++) {
+
 			System.out.println(day.charAt(i));
 		}
-		
+
 		System.out.println("we printed wednesday in reversed in for loop");
-		for (int i = day.length()-1 ; i >= 0 ; i--) {
-			
+		for (int i = day.length() - 1; i >= 0; i--) {
+
 			System.out.println(day.charAt(i));
 		}
+
+//		print wdedy
+
+		for (int i = 0; i <= day.length() - 1; i += 2) {
+
+			System.out.println(day.charAt(i));
+		}
+//	OR we can use if else condition
+
+		for(int i = 0 ; i<=day.length()-1 ; i++) {
+			if (i % 2 == 0) {
+		
+			System.out.println(day.charAt(i));
+		}}
 
 	}
 
