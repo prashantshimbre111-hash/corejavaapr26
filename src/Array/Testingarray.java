5126fc54bad369b88da2fdbaf30945044be50371
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 12:18:51 +0530

    ,

---
+++ src/Array/Testingarray.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class Testingarray {
+	
+	public static void main(String [] prashant) {
+		
+		int[] s = {11,33,44,33,55,44,66,99};
+		
+		System.out.println(s[4]);
+		
+		for (int i = 0 ; i < s.length ; i++){
+			
+			if(s[i]%2==0) {
+			
+			System.out.println(s[i]);}
+		}
+	}
+
+}
