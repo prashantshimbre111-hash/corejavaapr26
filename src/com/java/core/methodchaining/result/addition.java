0527832e3872f1f52ed6ed02117292192ad11679
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-14 14:02:17 +0530

    .

---
+++ src/com/java/core/methodchaining/result/addition.java
@@ -0,0 +1,13 @@
+package com.java.core.methodchaining.result;
+
+public class addition {
+	
+	public int add(int eng , int math , int sci , int his , int geo) {
+		
+		int sum = eng + math + sci + his + geo;
+		return sum;
+		
+		
+	}
+
+}
