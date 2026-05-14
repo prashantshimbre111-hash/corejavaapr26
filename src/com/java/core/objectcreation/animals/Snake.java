42c9829c3b65262affb39479fa8c3a49845d422d
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-14 13:33:27 +0530

    Object creation implemented

---
+++ src/com/java/core/objectcreation/animals/Snake.java
@@ -0,0 +1,11 @@
+package com.java.core.objectcreation.animals;
+
+public class Snake {
+	
+	public static String name = "snake";
+	public static int  age = 13;
+	public static String place = "amazon";
+	public static String colour = "green";
+	public static double sleeptimehr = 12.55;
+
+}
--- src/com/java/core/objectcreation/animals/animal.java
+++ src/com/java/core/objectcreation/animals/animal.java
@@ -3,30 +3,47 @@ package com.java.core.objectcreation.animals;
 public class animal {
 	public static void main(String[] args) {
 		lion l = new lion();
-		  System.out.println("name of animal : " + l.name);
-		  System.out.println("gender of the animal : " + l.gender);
-		  System.out.println("age of the animal : " + l.age);
-		  System.out.println("sleeptime of the animal : " + l.sleeptime);
-		  
-		  tiger t = new tiger();
-		  System.out.println("name of the animal : " + t.name);
-		  System.out.println("gender of the animal : " + t.gender);
-		  System.out.println("age of the animal : " + t.age);
-		  System.out.println("sleeptime of the animal : " + t.sleeptime);
-		  
-		  leoperd le = new leoperd();
-		  System.out.println("name of the animal : " + le.name);
-		  System.out.println("gender of the animal : " + le.gender);
-		  System.out.println("age of the animal : " + le.age);
-		  System.out.println("sleeptime of the animal : " + le.sleeptime);
-		  
-		  elephant e = new elephant();
-		  System.out.println("name of animal ; " + e.name);
-		  System.out.println("gender of animal : " + e.gender);
-		  System.out.println("age of animal : " + e.age);
-		  System.out.println("sleeptime of animal : " + e.sleeptime);
-		  System.out.println("is this tiger : " + e.tiger);
-		  
+
+		System.err.println("animal details are : ");
+		System.out.println("name of animal : " + l.name);
+		System.out.println("gender of the animal : " + l.gender);
+		System.out.println("age of the animal : " + l.age);
+		System.out.println("sleeptime of the animal : " + l.sleeptime);
+
+		tiger t = new tiger();
+		System.out.println(" ");
+		System.out.println("animal details are : ");
+		System.err.println("animal details are : ");
+		System.out.println("name of the animal : " + t.name);
+		System.out.println("gender of the animal : " + t.gender);
+		System.out.println("age of the animal : " + t.age);
+		System.out.println("sleeptime of the animal : " + t.sleeptime);
+
+		leoperd le = new leoperd();
+		System.out.println(" ");
+		System.err.println("animal details are : ");
+		System.out.println("name of the animal : " + le.name);
+		System.out.println("gender of the animal : " + le.gender);
+		System.out.println("age of the animal : " + le.age);
+		System.out.println("sleeptime of the animal : " + le.sleeptime);
+
+		elephant e = new elephant();
+		System.out.println(" ");
+		System.err.println("animal details are : ");
+		System.out.println("name of animal ; " + e.name);
+		System.out.println("gender of animal : " + e.gender);
+		System.out.println("age of animal : " + e.age);
+		System.out.println("sleeptime of animal : " + e.sleeptime);
+		System.out.println("is this tiger : " + e.tiger);
+
+		System.out.println(" ");
+		System.err.println("animal details are : ");
+		System.out.println("name of animal : " + Snake.name);
+		System.out.println("age of the animal : " + Snake.age);
+		System.out.println("colour of the animal : " + Snake.colour);
+		System.out.println("sleeptime of the animaal : " + Snake.sleeptimehr);
+		System.out.println("place of the animal : " + Snake.place);
+
 	}
 
 }
