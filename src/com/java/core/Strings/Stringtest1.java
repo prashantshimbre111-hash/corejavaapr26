cfb111fb81b28a2159ea64ce25e3df6f5d820792
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-14 14:45:01 +0530

    practice of non static method of string implemented.

---
+++ src/com/java/core/Strings/Stringtest1.java
@@ -0,0 +1,18 @@
+package com.java.core.Strings;
+
+public class Stringtest1 {
+	
+	public static void main(String [] args) {
+		
+		String name = "PrasHantnAndkishorShimbare";
+		
+		System.out.println(name.length());
+		System.out.println(name.charAt(6));
+		System.out.println(name.toLowerCase());
+		System.out.println(name.toUpperCase());
+		System.out.println(name.contains("P"));
+		System.out.println(name.concat("Parda"));
+		
+	}
+
+}
