0039bfba9f8a14c91f1dd6b8c5b79fc38492a699
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-14 14:52:42 +0530

    String practice implemented.

---
+++ src/com/java/core/Strings/Teststring1.java
@@ -0,0 +1,19 @@
+package com.java.core.Strings;
+
+public class Teststring1 {
+
+	public static void main(String [] args) {
+		
+		String name = "prashant";
+		
+		String name1 = new String("prashant");
+		
+		
+		System.out.println(name);
+		System.out.println(name1);
+		
+		System.out.println(name.equals(name1));
+		
+		System.out.println(name==name1);
+	}
+}
