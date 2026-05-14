122023331e44033d7ed4020a543f782f17ab3100
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-14 14:04:01 +0530

    Result application implemented with parameterized return type method and method chaining.

---
+++ src/com/java/core/methodchaining/result/Test.java
@@ -0,0 +1,13 @@
+package com.java.core.methodchaining.result;
+
+public class Test {
+	
+	public static void main(String [] prashant) {
+		
+		average a = new average();
+		int result = a.ave(87, 76, 89, 99, 87);
+		System.out.println("student's result : " + result);
+		
+	}
+
+}
