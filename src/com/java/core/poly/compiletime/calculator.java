c4d71a61bd2cddf69d0735e2e7151681cb8efa60
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-22 15:12:26 +0530

    Polymorphysm compile time practice implemented

---
+++ src/com/java/core/poly/compiletime/calculator.java
@@ -0,0 +1,13 @@
+package com.java.core.poly.compiletime;
+
+public class calculator {
+
+	public void sum(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	public void sum(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
+
+}
---
+++ src/com/java/core/poly/compiletime/test.java
@@ -0,0 +1,12 @@
+package com.java.core.poly.compiletime;
+
+public class test {
+	public static void main(String[] args) {
+		
+		calculator c = new calculator();
+		c.sum(12, 32);
+		c.sum(42, 12, 32);
+		
+	}
+
+}
