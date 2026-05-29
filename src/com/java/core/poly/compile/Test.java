a7b9f3a2829bccc19e043d09f3a0168e3d0efa92
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-29 12:23:36 +0530

    Compile time polymorphism(overloading) practice implemetned.

---
+++ src/com/java/core/poly/compile/Test.java
@@ -0,0 +1,11 @@
+package com.java.core.poly.compile;
+
+public class Test {
+	
+	public static void main(String [] args) {
+		
+		cal c = new cal();
+		c.sum(12, 12 , 1 , "prashant");
+	}
+
+}
---
+++ src/com/java/core/poly/compile/cal.java
@@ -0,0 +1,17 @@
+package com.java.core.poly.compile;
+
+public class cal {
+
+	public void sum(int a, int b) {
+		System.out.println(a + b);
+
+	}
+	public void sum(int a , int b , int c) {
+		System.out.println(a+b+c);
+	}
+	public void sum(int a , int b , int c , String s) {
+		
+		System.out.println((a+b+c) + s);
+	}
+
+}
