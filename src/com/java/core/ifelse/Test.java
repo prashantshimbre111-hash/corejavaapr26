30bf2bf743f8a86baf856b7fe9d9d9060480a4a8
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-06-10 14:58:30 +0530

    if else condition practice implemented

---
+++ src/com/java/core/ifelse/Test.java
@@ -0,0 +1,17 @@
+package com.java.core.ifelse;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+
+	cal c = new cal();
+	
+	if(c.a==0) {
+		c.getsub();
+	}
+	else {
+		c.getadd();
+	}
+}
+}
---
+++ src/com/java/core/ifelse/cal.java
@@ -0,0 +1,17 @@
+package com.java.core.ifelse;
+
+public class cal {
+	int a = 33;
+	int b = 55;
+	
+	public void getadd() {
+
+	
+		System.out.println(a+b);
+		
+	}
+	public void getsub() {
+		System.out.println(a-b);
+	}
+
+}
